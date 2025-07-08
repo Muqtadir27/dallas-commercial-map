89b85968ea4e718b7f1f36b63c189b5c538612bb
Mohammed Abdul Muqtadir <abdulmuqtadir1027@gmail.com>
2025-07-08 21:19:55 +0530

    Fix: Allow CORS for deployed frontend

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://dallas-map.onrender.com"])
 
 @app.route('/api/dallas-commercial-properties', methods=['GET'])
 def get_predicted_properties():
